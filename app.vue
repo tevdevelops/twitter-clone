742fdd4b5dff298584c9af369b62c28e070c1231
Tevin Rivera <tevdevelops@gmail.com>
2022-12-07 10:39:08 -0500

    add tailwind. start on left sidebar with icons

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1>Twitter Clone</h1>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- left sidebar -->
+          <div class="md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+
+          <!-- Main Content -->
+
+          <!-- Right Sidebar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const darkMode = ref(false)
+</script>
