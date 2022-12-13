89cab0bc76a1bbea358bc1c4e879301f51610925
Tevin Rivera <tevdevelops@gmail.com>
2022-12-13 10:11:21 -0500

    create FE for auth page. create func for logging in

---
+++ components/Auth/Form.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="pt-5 space-y-6">
+      <UIInput
+        v-model="data.username"
+        label="Username"
+        placeholder="@username"
+      />
+      <UIInput
+        v-model="data.password"
+        label="Password"
+        placeholder="password"
+        type="password"
+      />
+
+      <div><button @click="handleLogin">Login</button></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const data = reactive({
+  username: '',
+  password: '',
+  loading: false,
+})
+
+async function handleLogin() {
+  const { login } = useAuth()
+
+  data.loading = true
+
+  try {
+    await login({
+      username: data.username,
+      password: data.password,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  data.loading = false
+}
+</script>
